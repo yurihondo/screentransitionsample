41f4cf8ce948deb1b1b2c56085838c981b4d7cb2
yurihondo <yurihondo.dev@gmail.com>
2024-09-11 22:42:19 +0900

    Implement Edit feature

--- feature/applepie/src/main/java/com/yurihondo/screentransitionsample/applepie/ApplePieMr1Route.kt
+++ feature/applepie/src/main/java/com/yurihondo/screentransitionsample/applepie/ApplePieMr1Route.kt
@@ -8,11 +8,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.parameters.DeepLink
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.result.ResultRecipient
+import com.ramcosta.composedestinations.result.onResult
+import com.yurihondo.screentransitionsample.applepie.destinations.EditRouteDestination
 import com.yurihondo.screentransitionsample.applepie.navigation.ApplePieMr1Graph
 import com.yurihondo.screentransitionsample.applepie.navigation.ApplePieNavigator
 
@@ -32,11 +40,18 @@ data class ApplePieMr1Args(
 @Composable
 internal fun ApplePieMr1Route(
     applePieNavigator: ApplePieNavigator,
+    destinationsNavigator: DestinationsNavigator,
     args: ApplePieMr1Args,
+    resultRecipient: ResultRecipient<EditRouteDestination, String>
 ) {
+    var from by rememberSaveable { mutableStateOf(args.from) }
+
+    resultRecipient.onResult { editedFrom -> from = editedFrom }
+
     ApplePieMr1Screen(
-        from = args.from,
+        from = from,
         onClickMoveBananaBreadMr1 = applePieNavigator::navigateToBananaBreadMr1,
+        onNavigateEdit = { destinationsNavigator.navigate(EditRouteDestination(from)) }
     )
 }
 
@@ -44,6 +59,7 @@ internal fun ApplePieMr1Route(
 private fun ApplePieMr1Screen(
     from: String,
     onClickMoveBananaBreadMr1: () -> Unit,
+    onNavigateEdit: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
@@ -55,6 +71,10 @@ private fun ApplePieMr1Screen(
         Spacer(modifier = Modifier.height(32.dp))
         Button(onClick = onClickMoveBananaBreadMr1) {
             Text(text = "Jump to Banana Bread MR1")
+        }
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = onNavigateEdit) {
+            Text(text = "Edit")
         }
     }
 }
---
+++ feature/applepie/src/main/java/com/yurihondo/screentransitionsample/applepie/EditRoute.kt
@@ -0,0 +1,100 @@
+package com.yurihondo.screentransitionsample.applepie
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.result.ResultBackNavigator
+import com.ramcosta.composedestinations.spec.DestinationStyle
+import com.yurihondo.screentransitionsample.applepie.navigation.ApplePieMr1Graph
+
+@Destination<ApplePieMr1Graph>(
+    style = DestinationStyle.Dialog::class
+)
+@Composable
+internal fun EditRoute(
+    from: String,
+    resultBackNavigator: ResultBackNavigator<String>,
+) {
+    EditScreen(
+        initialValue = from,
+        onDone = {
+            resultBackNavigator.navigateBack(it)
+        },
+    )
+}
+
+@Composable
+private fun EditScreen(
+    initialValue: String,
+    onDone: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var textFieldValue by remember {
+        mutableStateOf(
+            TextFieldValue(
+                text = initialValue,
+                selection = TextRange(initialValue.length)
+            )
+        )
+    }
+    val focusManager = LocalFocusManager.current
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
+    Column(
+        modifier = modifier
+            .background(color = MaterialTheme.colorScheme.background)
+            .fillMaxWidth()
+            .padding(32.dp)
+            .focusRequester(focusRequester),
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Top,
+    ) {
+        Text(
+            style = MaterialTheme.typography.labelLarge,
+            text = "Edit your from"
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = textFieldValue,
+            onValueChange = { textFieldValue = it },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    onDone(textFieldValue.text)
+                    focusManager.clearFocus()
+                }
+            )
+        )
+    }
+}
